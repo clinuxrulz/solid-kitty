81767d79960ababcd13b03dcdfdbc6b17f7bc414
Clinton Selke <clinuxrulz@gmail.com>
2025-01-25 18:25:29 +1000

    wip

--- src/reactive-simulator/modes/AddLinkMode.ts
+++ src/reactive-simulator/modes/AddLinkMode.ts
@@ -1,19 +1,61 @@
 import { createStore } from "solid-js/store";
 import { Mode } from "../Mode";
 import { ModeParams } from "../ModeParams";
+import { Accessor, Component, createMemo } from "solid-js";
+import { Node } from "../Node";
+import { opToArr } from "../../util";
 
 export class AddLinkMode implements Mode {
+    instructions: Component;
+    highlightNodes: Accessor<Node[]>;
+    click: () => void;
+
     constructor(params: {
         modeParams: ModeParams,
         type: "Single" | "Double",
     }) {
+        let modeParams = params.modeParams;
+        let type = () => params.type;
         let [ state, setState, ] = createStore<{
             sourceNode: Node | undefined,
-            tarketNode: Node | undefined,
         }>({
             sourceNode: undefined,
-            tarketNode: undefined,
         });
-        
+        let selectableNodeUnderMouse = modeParams.pickingSystem.nodeUnderMouse;
+        //
+        this.instructions = () => {
+            return (() => {
+                if (state.sourceNode == undefined) {
+                    return "Select source node.";
+                } else {
+                    return "Select target node.";
+                }
+            }) + "\r\n" +
+            "(Press escape when you are done.)";
+        };
+        this.highlightNodes = createMemo(() => opToArr(selectableNodeUnderMouse()));
+        this.click = () => {
+            if (state.sourceNode == undefined) {
+                let node = selectableNodeUnderMouse();
+                if (node != undefined) {
+                    setState("sourceNode", node);
+                }
+                return;
+            }
+            let node = selectableNodeUnderMouse();
+            if (node != undefined) {
+                state.sourceNode.setState(
+                    "sinks",
+                    [ ...state.sourceNode.state.sinks, node, ],
+                );
+                if (type() == "Double") {
+                    node.setState(
+                        "sources",
+                        [ ...node.state.sources, state.sourceNode, ],
+                    );
+                }
+                setState("sourceNode", undefined);
+            }
+        };
     }
 }
