4fe7c620860b419a68bc7a8d1f4a3d7750b21d08
Clinton Selke <clinuxrulz@gmail.com>
2025-02-01 09:00:11 +1000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -9,34 +9,40 @@ const App: Component = (props) => {
             <div
                 style={{
                     "margin-left": "10px",
-                    "display": "flex",
-                    "flex-direction": "column",
+                    "display": "inline-block",
                 }}
             >
-                <button
-                    class="btn"
-                    onClick={() => navigate("/kitty-demo")}
+                <div
+                    style={{
+                        "display": "flex",
+                        "flex-direction": "column",
+                    }}
                 >
-                    Kitty Demo
-                </button><br/>
-                <button
-                    class="btn"
-                    onClick={() => navigate("/pixel-editor")}
-                >
-                    Pixel Editor
-                </button><br/>
-                <button
-                    class="btn"
-                    onClick={() => navigate("/colour-picker")}
-                >
-                    Colour Picker
-                </button>
-                <button
-                    class="btn"
-                    onClick={() => navigate("/reactive-sim")}
-                >
-                    Reactive Simulator
-                </button>
+                    <button
+                        class="btn"
+                        onClick={() => navigate("/kitty-demo")}
+                    >
+                        Kitty Demo
+                    </button><br/>
+                    <button
+                        class="btn"
+                        onClick={() => navigate("/pixel-editor")}
+                    >
+                        Pixel Editor
+                    </button><br/>
+                    <button
+                        class="btn"
+                        onClick={() => navigate("/colour-picker")}
+                    >
+                        Colour Picker
+                    </button>
+                    <button
+                        class="btn"
+                        onClick={() => navigate("/reactive-sim")}
+                    >
+                        Reactive Simulator
+                    </button>
+                </div>
             </div>
         </div>
     );
