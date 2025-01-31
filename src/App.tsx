bbbd6cc0bca5d3e777e2c418a09cf36491053519
Clinton Selke <clinuxrulz@gmail.com>
2025-02-01 08:58:04 +1000

    trying out unocss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import { A } from "@solidjs/router";
+import { A, useNavigate } from "@solidjs/router";
 import { Component } from "solid-js";
 
 const App: Component = (props) => {
+    let navigate = useNavigate();
     return (
         <div style="flex-grow: 1; overflow: auto; ">
-            <b>Links:</b>
-            <ul>
-                <li><A href="/kitty-demo">Kitty Demo</A></li>
-                <li><A href="/pixel-editor">Pixel Editor</A></li>
-                <li><A href="/colour-picker">Colour Picker</A></li>
-                <li><A href="/reactive-sim">Reactive Simulator</A></li>
-            </ul>
+            <b>Links:</b><br/>
+            <div
+                style={{
+                    "margin-left": "10px",
+                    "display": "flex",
+                    "flex-direction": "column",
+                }}
+            >
+                <button
+                    class="btn"
+                    onClick={() => navigate("/kitty-demo")}
+                >
+                    Kitty Demo
+                </button><br/>
+                <button
+                    class="btn"
+                    onClick={() => navigate("/pixel-editor")}
+                >
+                    Pixel Editor
+                </button><br/>
+                <button
+                    class="btn"
+                    onClick={() => navigate("/colour-picker")}
+                >
+                    Colour Picker
+                </button>
+                <button
+                    class="btn"
+                    onClick={() => navigate("/reactive-sim")}
+                >
+                    Reactive Simulator
+                </button>
+            </div>
         </div>
     );
 };
--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,10 @@ import presetUno from "unocss/preset-uno";
 
 export default defineConfig({
 	presets: [ presetUno(), ],
+    shortcuts: [
+        { "btn": `bg-blue-400 text-blue-100 py-2 px-4 rounded-lg`, },
+        [/^btn-(.*)$/, ([, c]) => `bg-${c}-400 text-${c}-100 py-2 px-4 rounded-lg`],
+    ],
     rules: [
     ],
 });
