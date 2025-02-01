f6c01c17b3872e678eaf6bbd3349b27958aa1311
Clinton Selke <clinuxrulz@gmail.com>
2025-02-01 15:49:39 +1000

    wip levelbuilder

--- src/level-builder/LevelBuilder.tsx
+++ src/level-builder/LevelBuilder.tsx
@@ -1,18 +1,44 @@
-import { Component } from "solid-js";
+import { Component, Switch } from "solid-js";
+import { Vec2 } from "../Vec2";
+import { createStore } from "solid-js/store";
+
+type State = {
+    selectedTab: "Tilesets" | "Levels",
+    tilesets: {
+        name: string,
+        image: HTMLImageElement,
+        blockTable: {
+            name: string,
+            pos: Vec2,
+            size: Vec2,
+        }[],
+    }[],
+};
 
 const LevelBuilder: Component = () => {
+    let [ state, setState, ] = createStore<State>({
+        selectedTab: "Tilesets",
+        tilesets: [],
+    });
     return (
-        <div style="flex-grow: 1;">
-            <b>Test Tabs:</b><br/>
+        <div style={{
+            "flex-grow": "1",
+            "display": "flex",
+            "flex-direction": "column",
+            "overflow": "hidden",
+        }}>
             <ul class="nav-tabs">
                 <li class="nav-item">
-                    <a href="#" class="nav-link-selected">Tab 1</a>
+                    <button
+                        class="nav-link-selected"
+                    >
+                        Tilesets
+                    </button>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">Tab 2</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Tab 3</a>
+                    <button class="nav-link">
+                        Levels
+                    </button>
                 </li>
             </ul>
         </div>
