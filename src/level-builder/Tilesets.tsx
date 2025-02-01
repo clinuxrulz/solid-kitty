5eb2a65790d94f036de07df1f8bcb335c84d52a2
Clinton Selke <clinuxrulz@gmail.com>
2025-02-01 16:09:42 +1000

    wip

--- src/level-builder/Tilesets.tsx
+++ src/level-builder/Tilesets.tsx
@@ -31,7 +31,36 @@ export class Tilesets {
     }> = (props) => {
         return (
             <div style={props.style}>
-                tilesets
+                <div
+                    style={{
+                        "width": "100%",
+                        "height": "100%",
+                        "overflow": "hidden",
+                        "display": "flex",
+                        "flex-direction": "row",
+                    }}
+                >
+                    <div>
+                        list of tilesets
+                    </div>
+                    {/* a visual divider */}
+                    <div
+                        style={{
+                            "border-radius": "3px",
+                            "width": "6px",
+                            "height": "100%",
+                            "background-color": "#bbb",
+                        }}
+                    />
+                    {/* the current tileset content */}
+                    <div
+                        style={{
+                            "flex-grow": "1",
+                        }}
+                    >
+                        tileset content
+                    </div>
+                </div>
             </div>
         );
     };
