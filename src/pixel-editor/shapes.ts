98488bba3608d6420f4a7063672bf5a2c1b78e3e
Clinton Selke <clinuxrulz@gmail.com>
2025-02-09 00:23:51 +1000

    eliminated square roots from ellipse drawing code

--- src/pixel-editor/shapes.ts
+++ src/pixel-editor/shapes.ts
@@ -54,7 +54,6 @@ export function drawLine(
     }
 }
 
-// untested
 export function drawEllipse(
   centerX: number,
   centerY: number,
@@ -64,11 +63,13 @@ export function drawEllipse(
 ): void {
     let x = 0;
     let y = -radiusY;
-    let a = Math.sqrt(radiusY);
-    let b = Math.sqrt(radiusX);
+    let a = radiusY;
+    let b = radiusX;
     let errL1 = 0;
-    let errL2x = a;
-    let errL2y = 2*b*y + b;
+    let errL2x = a*a;
+    let errL2y = 2*b*b*y + b*b;
+    let twoA2 = 2*a*a;
+    let twoB2 = 2*b*b;
     let draw4 = () => {
         drawPixel(centerX + x, centerY + y);
         drawPixel(centerX - x, centerY + y);
@@ -78,12 +79,12 @@ export function drawEllipse(
     while (y < 0) {
         draw4();
         errL1 += errL2x;
-        errL2x += a + a;
+        errL2x += twoA2;
         ++x;
         while (errL1 > 0) {
             errL1 += errL2y;
-            errL2y += b + b;
-            y++;
+            errL2y += twoB2;
+            ++y;
             draw4();
             if (y >= 0) {
                 return;
