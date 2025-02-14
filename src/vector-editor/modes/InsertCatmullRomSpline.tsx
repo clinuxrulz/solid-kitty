6a872590dda53b8cf1a1ba4e17a34cecd30d8c5b
Clinton Selke <clinuxrulz@gmail.com>
2025-02-14 16:09:20 +1000

    wip

--- src/vector-editor/modes/InsertCatmullRomSpline.tsx
+++ src/vector-editor/modes/InsertCatmullRomSpline.tsx
@@ -1,12 +1,25 @@
-import { Component } from "solid-js";
+import { Component, createMemo } from "solid-js";
 import { Mode } from "../Mode";
 import { ModeParams } from "../ModeParams";
+import { createStore } from "solid-js/store";
+import { Vec2 } from "../../Vec2";
 
 export class InsertCatmullRomSpline implements Mode {
     instructions: Component;
-
+    click: () => void;
     constructor(modeParams: ModeParams) {
-
+        let [ state, setState, ] = createStore<{
+            controlPoints: Vec2[],
+        }>({
+            controlPoints: [],
+        });
+        let workingPt = createMemo(() => {
+            let mousePos = modeParams.mousePos();
+            if (mousePos == undefined) {
+                return undefined;
+            }
+            return modeParams.screenPtToWorldPt(mousePos);
+        });
         let doInsert: () => void = () => {};
         this.instructions = () => {
             return (<>
@@ -18,6 +31,12 @@ export class InsertCatmullRomSpline implements Mode {
                     End Mode
                 </button>
             </>);
+        };
+        this.click = () => {
+            let pt = workingPt();
+            if (pt != undefined) {
+                setState("controlPoints", (x) => [...x, pt]);
+            }
         };
     }
 }
