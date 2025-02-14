5a9158b364a0116058228ca8fc12a790df687a7b
Clinton Selke <clinuxrulz@gmail.com>
2025-02-14 16:17:14 +1000

    wip

--- src/vector-editor/modes/InsertCatmullRomSpline.tsx
+++ src/vector-editor/modes/InsertCatmullRomSpline.tsx
@@ -1,8 +1,10 @@
-import { Component, createMemo } from "solid-js";
+import { Accessor, Component, createComputed, createMemo, onCleanup, untrack } from "solid-js";
 import { Mode } from "../Mode";
 import { ModeParams } from "../ModeParams";
 import { createStore } from "solid-js/store";
 import { Vec2 } from "../../Vec2";
+import { catmullRomSplineComponentType } from "../components/CatmullRomSpline";
+import { opToArr } from "../../kitty-demo/util";
 
 export class InsertCatmullRomSpline implements Mode {
     instructions: Component;
@@ -20,7 +22,39 @@ export class InsertCatmullRomSpline implements Mode {
             }
             return modeParams.screenPtToWorldPt(mousePos);
         });
+        let wipControlPoints = createMemo(() => {
+            if (state.controlPoints.length == 0) {
+                return undefined;
+            }
+            let workingPt2 = workingPt();
+            if (state.controlPoints.length == 1 && workingPt2 == undefined) {
+                return undefined;
+            }
+            return [ ...state.controlPoints, ...opToArr(workingPt2), ];
+        });
         let doInsert: () => void = () => {};
+        {
+            let hasWipControlPoints = createMemo(() =>
+                wipControlPoints() != undefined
+            );
+            createComputed(() => {
+                if (!hasWipControlPoints()) {
+                    return;
+                }
+                let wipControlPoints2 = wipControlPoints as Accessor<NonNullable<ReturnType<typeof wipControlPoints>>>;
+                let world = modeParams.world();
+                let spline = catmullRomSplineComponentType.create({
+                    controlPoints: untrack(wipControlPoints2),
+                    isClosed: false,
+                })
+                let entity = untrack(() => world.createEntity([
+                    spline,
+                ]));
+                onCleanup(() => {
+                    world.destroyEntity(entity);
+                });
+            });
+        }
         this.instructions = () => {
             return (<>
                 Click in the control points for your curve.<br/>
