6c3b2f3592d399d5fb1be56d85341bc676745370
Clinton Selke <clinuxrulz@gmail.com>
2025-02-22 15:37:50 +1000

    wip

--- src/level-builder/components/registry.ts
+++ src/level-builder/components/registry.ts
@@ -1,6 +1,7 @@
 import { EcsRegistry } from "../../ecs/EcsRegistry";
 import { frameComponentType } from "./FrameComponent";
 import { registry as baseRegistry } from "../../ecs/components/registry";
+import { tilesetComponentType } from "./TilesetComponent";
 
 export const registry = new EcsRegistry([
     ...baseRegistry.componentTypes,
