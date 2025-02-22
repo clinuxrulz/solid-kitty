bd6159f5bafe8a757eba5b82c872d2284ebdbc30
Clinton Selke <clinuxrulz@gmail.com>
2025-02-22 12:40:10 +1000

    form when clicking floating edit data button

--- src/level-builder/texture-atlas/modes/EditDataMode.tsx
+++ src/level-builder/texture-atlas/modes/EditDataMode.tsx
@@ -1,13 +1,37 @@
+import { Component } from "solid-js";
 import { EcsComponent } from "../../../ecs/EcsComponent";
 import { FrameState } from "../components/FrameComponent";
 import { Mode } from "../Mode";
 import { ModeParams } from "../ModeParams";
 
 export class EditDataMode implements Mode {
+    overlayHtmlUI: Component;
+
     constructor(params: {
         modeParams: ModeParams,
         frameComponent: EcsComponent<FrameState>,
     }) {
-
+        let modeParams = params.modeParams;
+        let frameComponent = params.frameComponent;
+        this.overlayHtmlUI = () => (
+            <div
+                style={{
+                    "position": "absolute",
+                    "left": "0",
+                    "top": "0",
+                    "right": "0",
+                    "bottom": "0",
+                    "background-color": "rgba(0,0,0,0.5)",
+                    "pointer-events": "auto",
+                }}
+            >
+                <button
+                    class="btn"
+                    onClick={() => modeParams.onDone()}
+                >
+                    Close
+                </button>
+            </div>
+        );
     }
 }
