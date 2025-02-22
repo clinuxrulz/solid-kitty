c7bae4797cc53727ad2d9141cc762f077860468f
Clinton Selke <clinuxrulz@gmail.com>
2025-02-22 13:43:22 +1000

    wip

--- src/level-builder/texture-atlas/modes/EditDataMode.tsx
+++ src/level-builder/texture-atlas/modes/EditDataMode.tsx
@@ -30,14 +30,40 @@ export class EditDataMode implements Mode {
                         "left": "50%",
                         "top": "50%",
                         "transform": "translate(-50%,-50%)",
+                        "background-color": "black",
+                        "border-radius": "10px",
+                        "padding": "20px",
                     }}
                 >
-                    <button
-                        class="btn"
-                        onClick={() => modeParams.onDone()}
-                    >
-                        Close
-                    </button>
+                    <table>
+                        <thead/>
+                        <tbody>
+                            <tr>
+                                <td style="padding-right: 10px;"><b>Name:</b></td>
+                                <td>
+                                    <input
+                                        type="text"
+                                        style="color: black;"
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br/>
+                    <div style="text-align: right;">
+                        <button
+                            class="btn"
+                            onClick={() => modeParams.onDone()}
+                        >
+                            OK
+                        </button>
+                        <button
+                            class="btn"
+                            onClick={() => modeParams.onDone()}
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </div>
             </div>
         );
