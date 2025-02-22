63fc121a9e70485203983075a659e5bf2f392555
Clinton Selke <clinuxrulz@gmail.com>
2025-02-22 15:42:37 +1000

    common sort order index component

---
+++ src/ecs/components/SortOrderIndexComponent.ts
@@ -0,0 +1,16 @@
+import { ok } from "../../kitty-demo/Result";
+import { EcsComponentType } from "../EcsComponent";
+
+export type SortOrderIndexState = {
+    orderIndex: number,
+};
+
+export const sortOrderIndexComponentType = new EcsComponentType<SortOrderIndexState>({
+    typeName: "SortOrderIndex",
+    toJson(value) {
+        return { orderIndex: value, };
+    },
+    fromJson(x) {
+        return ok({ orderIndex: x.orderIndex, });
+    }
+});
