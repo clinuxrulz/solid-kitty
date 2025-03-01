1d2e5e39ee744c3ac122067fde85e084dfd8095f
Clinton Selke <clinuxrulz@gmail.com>
2025-03-01 17:29:25 +1000

    wip

--- src/level-builder/components/registry.ts
+++ src/level-builder/components/registry.ts
@@ -2,9 +2,11 @@ import { EcsRegistry } from "../../ecs/EcsRegistry";
 import { frameComponentType } from "./FrameComponent";
 import { registry as baseRegistry } from "../../ecs/components/registry";
 import { textureAtlasComponentType } from "./TextureAtlasComponent";
+import { levelComponentType } from "./LevelComponent";
 
 export const registry = new EcsRegistry([
     ...baseRegistry.componentTypes,
     frameComponentType,
+    levelComponentType,
     textureAtlasComponentType,
 ]);
