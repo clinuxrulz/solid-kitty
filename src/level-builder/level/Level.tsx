92c55ca9f50d5b225198339a437379ace5a5da87
Clinton Selke <clinuxrulz@gmail.com>
2025-03-01 17:34:05 +1000

    wip

--- src/level-builder/level/Level.tsx
+++ src/level-builder/level/Level.tsx
@@ -529,12 +529,7 @@ export class Level {
                             ref={setSvg}
                             style={{
                                 "flex-grow": "1",
-                                "background-color": "#DDD",
-                                "background-image":
-                                    "linear-gradient(45deg, #FFFFFF 25%, transparent 25%), linear-gradient(-45deg, #FFFFFF 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #FFFFFF 75%), linear-gradient(-45deg, transparent 75%, #FFFFFF 75%)",
-                                "background-size": "20px 20px",
-                                "background-position":
-                                    "0 0, 0 10px, 10px -10px, -10px 0px",
+                                "background-color": "#FFF",
                                 "touch-action": "none",
                             }}
                             onWheel={onWheel}
