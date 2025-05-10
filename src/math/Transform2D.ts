a243332cd399c08bdd72fb47c5157f53e10b06fd
Clinton Selke <clinuxrulz@gmail.com>
2025-05-11 09:08:24 +1000

    wip

--- src/math/Transform2D.ts
+++ src/math/Transform2D.ts
@@ -2,9 +2,8 @@ import { Vec2 } from "./Vec2";
 import { Complex } from './Complex';
 
 export class Transform2D {
-    /*private*/ _origin : Vec2;
-
-    /*private*/ _orientation : Complex;
+    readonly origin : Vec2;
+    readonly orientation : Complex;
 
     public static identity : Transform2D = new Transform2D(
         Vec2.zero(),
@@ -12,65 +11,48 @@ export class Transform2D {
     );
 
     constructor(origin : Vec2, orientation : Complex) {
-        this._origin = origin;
-        this._orientation = orientation;
+        this.origin = origin;
+        this.orientation = orientation;
     }
 
     public static create(origin : Vec2, orientation : Complex): Transform2D {
         return new Transform2D(origin, orientation);
     }
 
-    public get origin(): Vec2 {
-        return this._origin;
-    }
-
-    public get orientation(): Complex {
-        return this._orientation;
-    }
-
     public get u(): Vec2 {
-        return this._orientation.u;
+        return this.orientation.u;
     }
 
     public get v(): Vec2 {
-        return this._orientation.v;
+        return this.orientation.v;
     }
-
-    public getOrigin() : Vec2 {
-        return this._origin;
-    }
-
-    public getOrientation() : Complex {
-        return this._orientation;
-    }
-
     public pointFromSpace(p : Vec2) : Vec2 {
-        return this._orientation.rotate(p).add(this._origin);
+        return this.orientation.rotate(p).add(this.origin);
     }
 
     public pointToSpace(p : Vec2) : Vec2 {
-        return this._orientation.conjugate().rotate(p.sub(this._origin));
+        return this.orientation.conjugate().rotate(p.sub(this.origin));
     }
 
     public vectorFromSpace(v : Vec2) : Vec2 {
-        return this._orientation.rotate(v);
+        return this.orientation.rotate(v);
     }
 
     public vectorToSpace(v : Vec2) : Vec2 {
-        return this._orientation.conjugate().rotate(v);
+        return this.orientation.conjugate().rotate(v);
     }
 
     public transformFromSpace(a : Transform2D) : Transform2D {
         return Transform2D.create(
-            this.pointFromSpace(a.getOrigin()),
-            a.getOrientation().times(this._orientation),
+            this.pointFromSpace(a.origin),
+            a.orientation.times(this.orientation),
         );
     }
 
     public transformToSpace(a : Transform2D) : Transform2D {
         return Transform2D.create(
-            this.pointToSpace(a.getOrigin()),
-            this.getOrientation().conjugate().times(a.getOrientation()),
+            this.pointToSpace(a.origin),
+            this.orientation.conjugate().times(a.orientation),
         );
     }
 }
