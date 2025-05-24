b774e2d8d1592a395f76f7fe64870e47e31be115
Clinton Selke <clinuxrulz@gmail.com>
2025-05-24 18:47:01 +1000

    tile / sprite collision detection tweak

--- src/systems/CollisionSystem.ts
+++ src/systems/CollisionSystem.ts
@@ -1,17 +1,73 @@
 import { levelRefComponentType } from "../components/LevelRefComponent";
 import { EcsWorld } from "../ecs/EcsWorld";
 import { createMemo, on, onCleanup } from "solid-js";
-import { Complex, createGetLevelsFolder, spriteComponentType, Transform2D, transform2DComponentType, Vec2 } from "../lib";
+import { Complex, createGetLevelsFolder, createTextureAtlasWithImageAndFramesList, scaleComponentType, spriteComponentType, Transform2D, transform2DComponentType, Vec2 } from "../lib";
 import { EcsWorldAutomergeProjection } from "../ecs/EcsWorldAutomergeProjection";
 import { registry } from "../level-builder/components/registry";
 import { levelComponentType } from "../level-builder/components/LevelComponent";
 import { Cont } from "../Cont";
 import { tileCollisionComponentType } from "../components/TileCollisionComponent";
+import { ReactiveCache } from "reactive-cache";
+import { FrameState } from "../level-builder/components/FrameComponent";
 
 export class CollisionSystem {
   constructor(params: { world: EcsWorld }) {
     let world = params.world;
     let levelsFolder = createGetLevelsFolder();
+    let textureAtlasWithImageAndFramesList =
+      createTextureAtlasWithImageAndFramesList();
+    let frameIdToFrameMap = createMemo(() => {
+      let textureAtlasWithImageAndFramesList2 =
+        textureAtlasWithImageAndFramesList();
+      if (textureAtlasWithImageAndFramesList2.type != "Success") {
+        return undefined;
+      }
+      let textureAtlasWithImageAndFramesList3 =
+        textureAtlasWithImageAndFramesList2.value;
+      let result = new Map<string, FrameState>();
+      for (let entry of textureAtlasWithImageAndFramesList3) {
+        for (let entry2 of entry.frames) {
+          result.set(entry2.frameId, entry2.frame);
+        }
+      }
+      return result;
+    });
+    let textureAtlasFilename_frameName_toFrameIdMap = createMemo(() => {
+      let textureAtlasWithImageAndFramesList2 =
+        textureAtlasWithImageAndFramesList();
+      if (textureAtlasWithImageAndFramesList2.type != "Success") {
+        return undefined;
+      }
+      let textureAtlasWithImageAndFramesList3 =
+        textureAtlasWithImageAndFramesList2.value;
+      let result = new Map<string, string>();
+      for (let entry of textureAtlasWithImageAndFramesList3) {
+        for (let entry2 of entry.frames) {
+          result.set(
+            entry.textureAtlasFilename() + "/" + entry2.frame.name,
+            entry2.frameId,
+          );
+        }
+      }
+      return result;
+    });
+    let lookupFrameIdByTextureAtlasFilenameAndFrameName_ = new ReactiveCache<
+      string | undefined
+    >();
+    let lookupFrameIdByTextureAtlasFilenameAndFrameName = (
+      textureAtlasFilename: string,
+      frameName: string,
+    ) =>
+      lookupFrameIdByTextureAtlasFilenameAndFrameName_.cached(
+        textureAtlasFilename + "/" + frameName,
+        () =>
+          textureAtlasFilename_frameName_toFrameIdMap()?.get(
+            textureAtlasFilename + "/" + frameName,
+          ),
+      );
+    let lookupFrameById_ = new ReactiveCache<FrameState | undefined>();
+    let lookupFrameById = (frameId: string) =>
+      lookupFrameById_.cached(frameId, () => frameIdToFrameMap()?.get(frameId));
     Cont.liftCC(
       on(levelsFolder, (levelsFolder) => {
         if (levelsFolder.type != "Success") {
@@ -124,19 +180,22 @@ export class CollisionSystem {
               return undefined;
             }
             let transform = world.getComponent(spriteEntity, transform2DComponentType)?.state.transform ?? Transform2D.identity;
+            let scale = world.getComponent(spriteEntity, scaleComponentType)?.state?.scale ?? 1.0;
             return {
               spriteEntity,
               sprite,
               transform,
+              scale,
             };
           }))
           .filterNonNullable()
-          .map(({ spriteEntity, sprite, transform, }) => ({
+          .map(({ spriteEntity, sprite, transform, scale, }) => ({
             level,
             shortIdToTextureAtlasAndFrameIdMap,
             spriteEntity,
             sprite,
             spriteTransform: transform,
+            spriteScale: scale,
           }));
       })
       .then(({
@@ -145,16 +204,26 @@ export class CollisionSystem {
           spriteEntity,
           sprite,
           spriteTransform,
+          spriteScale,
       }) => Cont.liftCC(() => {
         const spritePos = spriteTransform.origin;
         const tileWidth = 16*3;
         const tileHeight = 16*3;
+        let frameId = lookupFrameIdByTextureAtlasFilenameAndFrameName(
+          sprite.textureAtlasFilename,
+          sprite.frameName,
+        );
+        if (frameId == undefined) {
+          return;
+        }
+        let frame = lookupFrameById(frameId);
+        if (frame == undefined) {
+          return;
+        }
         const minXIdx = Math.floor(spritePos.x / tileWidth);
         const minYIdx = Math.floor(spritePos.y / tileHeight);
-        // just rough it for now
-        const maxXIdx = minXIdx;
-        const maxYIdx = minYIdx;
-        // TODO:
+        const maxXIdx = Math.max(minXIdx, Math.ceil((spritePos.x + frame.size.x * spriteScale) / tileWidth) - 1);
+        const maxYIdx = Math.max(minYIdx, Math.ceil((spritePos.y + frame.size.y * spriteScale) / tileHeight) - 1);
         for (let i = minYIdx; i <= maxYIdx; ++i) {
           let row = level.mapData[i];
           if (row == undefined) {
