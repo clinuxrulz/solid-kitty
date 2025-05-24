ecf01681af7d10837b1ab5c4333843a5d9b7ce7f
Clinton Selke <clinuxrulz@gmail.com>
2025-05-24 20:24:55 +1000

    missing types

---
+++ types/components/Velocity2DComponent.d.ts
@@ -0,0 +1,5 @@
+import { EcsComponentType, Vec2 } from '../lib';
+export type Velocity2DState = {
+    velocity: Vec2;
+};
+export declare const velocity2DComponentType: EcsComponentType<Velocity2DState>;
