15c0fcf5a48e1e0159177e5410f9e6f418ad2b47
Clinton Selke <clinuxrulz@gmail.com>
2025-06-08 08:12:31 +1000

    wip

--- src/automerge-doc-mutable-proxy.ts
+++ src/automerge-doc-mutable-proxy.ts
@@ -108,6 +108,122 @@ export function projectMutableOverAutomergeDocV2<T extends object>(
   return result;
 }
 
+export function projectMutableOverAutomergeDocArrayV2<T extends object>(
+  json: any,
+  updateJson: (cb: (json: any) => any) => void,
+  typeSchema: TypeSchema<T>
+): T {
+  if (typeof typeSchema != "object" || typeSchema.type != "Array") {
+    throw new Error("Expected object.");
+  }
+  let elementSchema = typeSchema.element;
+  let result = new Proxy<T>(
+    [undefined] as T,
+    {
+      defineProperty(target, property, attributes) {
+        console.log("defineProperty", target, property, attributes);
+        return Reflect.defineProperty(target, property, attributes);
+      },
+      deleteProperty(target, p) {
+        console.log("deleteProperty", target, p);
+        return Reflect.deleteProperty(target, p);
+      },
+      getOwnPropertyDescriptor(target, p) {
+        console.log("getOwnPropertyDescriptor", target, p);
+        let idx = (typeof p == "string") ? Number.parseInt(p) : undefined;
+        if (Number.isNaN(idx)) {
+          idx = undefined;
+        }
+        if (idx != undefined) {
+          let r = Reflect.getOwnPropertyDescriptor(target, 0);
+          if (r == undefined) {
+            return;
+          }
+          r = { ...r, };
+          r.value = (result as any)[p];
+          return r;
+        }
+        return Reflect.getOwnPropertyDescriptor(target, p);
+      },
+      getPrototypeOf(target) {
+        console.log("getPrototypeOf", target);
+        return Reflect.getPrototypeOf(target);
+      },
+      has(target, p) {
+        console.log("has", target);
+        let idx = (typeof p == "string") ? Number.parseInt(p) : undefined;
+        if (Number.isNaN(idx)) {
+          idx = undefined;
+        }
+        if (idx != undefined) {
+          return 0 <= idx && idx < json.length;
+        }
+        return Reflect.has(target, p);
+      },
+      isExtensible(target) {
+        console.log("isExtendable", target);
+        return Reflect.isExtensible(target);
+      },
+      ownKeys(target) {
+        console.log("ownKeys", target);
+        return [
+          ...Array(json.length).fill(undefined).map((_,idx) => idx.toString()),
+          ...Reflect.ownKeys(target).filter((key) => key != "0"),
+        ];
+      },
+      preventExtensions(target) {
+        console.log("preventExtensions", target);
+        return Reflect.preventExtensions(target);
+      },
+      setPrototypeOf(target, v) {
+        console.log("setPrototypeOf", target, v);
+        return Reflect.setPrototypeOf(target, v);
+      },
+      get(target, p, receiver) {
+        if (typeof p == "symbol") {
+          return (target as any)[p];
+        }
+        if (typeof p == "string") {
+          let idx = (typeof p == "string") ? Number.parseInt(p) : undefined;
+          if (Number.isNaN(idx)) {
+            idx = undefined;
+          }
+          if (idx != undefined && 0 <= idx && idx < json.length) {
+            let r = loadFromJsonViaTypeSchema(
+              elementSchema as any,
+              json[idx]
+            );
+            if (r.type == "Err") {
+              return makeDefaultViaTypeSchema(elementSchema as any);
+            }
+            return r.value;
+          }
+        }
+        return Reflect.get(target, p, receiver);
+      },
+      set(target, p, newValue, receiver) {
+        if (typeof p == "symbol") {
+          (target as any)[p] = newValue;
+          return true;
+        }
+        if (typeof p == "string") {
+          let idx = (typeof p == "string") ? Number.parseInt(p) : undefined;
+          if (Number.isNaN(idx)) {
+            idx = undefined;
+          }
+          if (idx != undefined) {
+            let r = saveToJsonViaTypeSchema(elementSchema as any, newValue);
+            updateJson((json2) => json2[p] = r);
+            return true;
+          }
+        }
+        return Reflect.set(target, p, newValue, receiver);
+      },
+    },
+  );
+  return result;
+}
+
 let wrappedMap = new WeakMap<any, any>();
 
 function isWrapped(x: any): boolean {
