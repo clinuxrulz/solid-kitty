407c51a861e91556ba8841f73f34b40ee281fb15
Clinton Selke <clinuxrulz@gmail.com>
2025-06-08 17:00:21 +1000

    updating types

--- types/components/TileCollisionComponent.d.ts
+++ types/components/TileCollisionComponent.d.ts
@@ -4,5 +4,7 @@ export type TileCollisionState = {
     frameName: string;
     width: number;
     height: number;
+    /** metaData is a json value */
+    metaData: any;
 };
 export declare const tileCollisionComponentType: EcsComponentType<TileCollisionState>;
